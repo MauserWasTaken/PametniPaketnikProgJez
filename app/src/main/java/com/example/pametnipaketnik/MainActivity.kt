8bd550e414e94189fdcc638e4098143202ff8585
MauserWasTaken <klemenm02@gmail.com>
2024-05-31 14:15:19 +0200

    added basic functionality for qr scanner

--- app/src/main/java/com/example/pametnipaketnik/MainActivity.kt
+++ app/src/main/java/com/example/pametnipaketnik/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import com.journeyapps.barcodescanner.ScanOptions
+import com.journeyapps.barcodescanner.ScanContract
+import com.journeyapps.barcodescanner.ScanIntentResult
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +18,26 @@ class MainActivity : AppCompatActivity() {
 
         // Nastavi OnClickListener za gumb
         qrScannerButton.setOnClickListener {
-            Toast.makeText(this, "Odpiranje QR scannerja", Toast.LENGTH_SHORT).show()
+            startQRScanner()
+        }
+    }
+
+    // Funkcija za zagon QR skenerja
+    private fun startQRScanner() {
+        val options = ScanOptions()
+        options.setPrompt("Scan a QR code")
+        options.setBeepEnabled(true)
+        options.setOrientationLocked(false)
+        qrCodeLauncher.launch(options)
+    }
+
+    private val qrCodeLauncher = registerForActivityResult(ScanContract()) { result: ScanIntentResult ->
+        if (result.contents != null) {
+            // QR koda uspešno skenirana
+            Toast.makeText(this, "Scanned: ${result.contents}", Toast.LENGTH_LONG).show()
+        } else {
+            // QR koda ni skenirana
+            Toast.makeText(this, "Canceled", Toast.LENGTH_LONG).show()
         }
     }
 }
